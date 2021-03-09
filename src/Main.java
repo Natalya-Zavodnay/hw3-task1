c0d5a6ea0a6daf18f1a71ede749e36b29cba0c6c
Natalya-Zavodnay <nz41330@gmail.com>
2021-03-09 22:41:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int ticketPrice = 10_000_00;
+        int mile = 20;
+        int total = ticketPrice / mile / 100;
+        System.out.println(total);
+    }
+}
